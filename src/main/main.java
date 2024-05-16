f0c014c36ddbb03ce0486f201b504084d96a3c12
victorjoaonull <contato_victorjoao@yahoo.com>
2024-05-15 22:22:09 -0300

    Projeto Calculadora 1.2.0 - Implementado interface para usuário utilizar

--- src/main/main.java
+++ src/main/main.java
@@ -1,12 +1,11 @@
 package main;
 
-import main.model.Calculos;
+import main.viewer.interfaceCalculadora;
 
 public class main {
     public static void main(String args[])
     {
-    Calculos c = new Calculos();
-    
-    System.out.println(c.calculadora("5510"));
+    interfaceCalculadora tela = new interfaceCalculadora();
+    tela.setVisible(true);
     }
 }
