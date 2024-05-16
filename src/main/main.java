e6a3b12fb0f0276adf679321a9766cd545048a17
victorjoaonull <contato_victorjoao@yahoo.com>
2024-05-15 21:52:33 -0300

    Projeto Calculadora 1.0.0 - Criado o model base para iniciar a parte da interface de usuário

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+import main.model.Calculos;
+
+public class main {
+    public static void main(String args[])
+    {
+    Calculos c = new Calculos();
+    
+    System.out.println(c.calculadora("5510"));
+    }
+}
